fefe4d0d52d6475d8d0d0638ee3d8517024d16f6
CamiBacan <cesepulveda@miuandes.cl>
2018-05-22 21:59:11 -0400

    Pregunta 1

---
+++ Lab10/Lab10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly archivoAssembly = Assembly.LoadFile(@"C:\Users\csepu\Desktop\municipality-library-CamiBacan\ClassLibrary1.dll");
+            foreach (Type type in archivoAssembly.GetTypes())
+            {
+                if (type.IsClass)
+                {
+                    Console.WriteLine("Clase: " + type.Name);
+
+                    foreach (PropertyInfo att in type.GetProperties())
+                    {
+                        Console.WriteLine("\tAtributos: " + att.Name + "({0})", att.PropertyType.Name);
+                    }
+                    foreach (MethodInfo meth in type.GetMethods())
+                    {
+                        Console.WriteLine("\t\tMetodos: " + meth.Name);
+                        ParameterInfo[] parameters = meth.GetParameters();
+                        Console.WriteLine("\t\tParametros: ");
+                        foreach (ParameterInfo par in parameters)
+                        {
+                            Console.WriteLine("\t\t\t" + par.ParameterType.Name);
+                        }
+                        Console.WriteLine("\t\tRetorna: " + meth.ReturnType.Name);
+                        Console.WriteLine("\n\n");
+                    }
+                    
+                
+                }
+                else if (type.IsInterface)
+                {
+                    Console.WriteLine("Interfaz: " + type.Name);
+
+
+                }
+
+                
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
